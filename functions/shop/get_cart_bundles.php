ad1a9020416df5ef8a9c4f9c6503ed574990ce6e
Nigel Powell <nigel@thesadsongco.com>
2025-05-04 07:35:23 +0100

    Bundle item options show in cart

--- functions/shop/get_cart_bundles.php
+++ functions/shop/get_cart_bundles.php
@@ -5,12 +5,19 @@ function getCartBundles($bundles, $db) {
     foreach($bundles as $cart_bundle) {
         $query = "SELECT bundle_id, price FROM Bundles WHERE bundle_id = ?";
         $bundle = $db->query($query, [$cart_bundle['bundle_id']])->fetch();
-        $query = "SELECT
-        Items.item_id, Items.name, Items.image
-        FROM Bundle_items
-        JOIN Items ON Bundle_items.item_id = Items.item_id
-        WHERE Bundle_items.bundle_id = ?";
-        $bundle['items'] = $db->query($query, [$cart_bundle['bundle_id']])->fetchAll();
+        foreach($cart_bundle['items'] as &$item) {
+            $query = "SELECT
+            Items.item_id, Items.name, Items.image
+            FROM Items
+            WHERE Items.item_id = ?";
+            $item_details = $db->query($query, [$item['item_id']])->fetch();
+            if ($item['option_id']) {
+                $query = "SELECT option_name FROM Item_options WHERE item_option_id = ?";
+                $item_details['option_name'] =  $db->query($query, [$item['option_id']])->fetch()['option_name'];
+                $item_details['option_id'] = $item['option_id'];
+            }
+            $bundle['items'][] = $item_details;
+        }
         $bundle['price'] = number_format($bundle['price'], 2);
         $cart_bundles[] = [...$bundle, "quantity"=>$cart_bundle['quantity']];
     }
