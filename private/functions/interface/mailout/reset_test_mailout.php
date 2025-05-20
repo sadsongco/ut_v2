58f6be4e0731db2ae6f958adbd640bad782e135b
Nigel Powell <nigel@thesadsongco.com>
2025-05-20 15:18:00 +0100

    Reset test mailing list functionality

--- private/functions/interface/mailout/reset_test_mailout.php
+++ private/functions/interface/mailout/reset_test_mailout.php
@@ -1,13 +1,18 @@
 <?php
 
+include(__DIR__ . "/../../../../functions/functions.php");
+require_once(base_path("classes/Database.php"));
 include_once("includes/mailout_includes.php");
+
+use Database\Database;
+$db = new Database('admin');
 
 $cond = "";
 if (isset($_GET['nigel'])) $cond = " WHERE `email` LIKE '%sadsongco%'";
 
 try {
     $query = "UPDATE test_mailing_list SET last_sent = 0$cond";
-    $stmt = $db->query($query);
+    $db->query($query);
 }
 catch (PDOException $e) {
     exit("Couldn't reset test mailing list: ".$e->getMessage());
