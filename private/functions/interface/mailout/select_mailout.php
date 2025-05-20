2ca3e25cd0442febb94ea177776463d877c02f01
Nigel Powell <nigel@thesadsongco.com>
2025-05-20 15:35:52 +0100

    Bug fixing select_mailout 2

--- private/functions/interface/mailout/select_mailout.php
+++ private/functions/interface/mailout/select_mailout.php
@@ -24,15 +24,11 @@ function getCompletedEmails($db, $table, $current_mailout) {
         $result = $db->query($query)->fetchAll(PDO::FETCH_ASSOC);
         $all = $result[0]['total'];
         $query = "SELECT COUNT(*) AS `sent` FROM `$table` WHERE `last_sent` = ?";
-        $stmt = $db->prepare($query);
-        $stmt->execute([$current_mailout]);
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $sent = $result[0]['sent'];
+
+        $result = $db->query($query, [$current_mailout])->fetch();
+        $sent = $result['sent'];
         $query = "SELECT COUNT(*) AS `errors`, IF(COUNT(*) > 0, 1, NULL) AS `error_flag` FROM `$table` WHERE `error`=?";
-        $stmt = $db->prepare($query);
-        $stmt->execute([1]);
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $errors = $result[0];
+        $errors = $db->query($query, [1])->fetch();
     }
     catch (PDO_EXCEPTION $e) {
         echo "Error retrieving completed emails: ".$e->getMessage();
