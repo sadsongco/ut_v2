36b4f987f913c48acd4f8c43ea974e186060d53a
Nigel Powell <nigel@thesadsongco.com>
2025-05-22 09:54:30 +0100

    Carousel admin and styling

--- controllers/index.php
+++ controllers/index.php
@@ -1,12 +1,16 @@
 <?php
 
+include(base_path("private/functions/utility/nl2p.php"));
+
 use Database\Database;
 
 $db = new Database();
 
-$carousel_tiles = $db->query("SELECT * FROM carousel ORDER BY tile_order ASC")->fetchAll();
+$carousel_tiles = $db->query("SELECT img_url, tile_title, tile_text FROM carousel ORDER BY tile_order ASC")->fetchAll();
 foreach ($carousel_tiles as &$tile) {
     $tile['path'] = CAROUSEL_ASSET_PATH . "images/" . $tile['img_url'];
+    $tile['tile_text'] = nl2p($tile['tile_text']);
 }
 
-echo $this->renderer->render('index', ['carousel_tiles'=>$carousel_tiles, 'nav'=>$this->nav]);
+echo $this->renderer->render('index', ['carousel_tiles'=>$carousel_tiles]);
+
--- private/functions/interface/content/update_carousel.php
+++ private/functions/interface/content/update_carousel.php
@@ -1,6 +1,7 @@
 <?php
 
 include(__DIR__ . "/../../../../functions/functions.php");
+include(base_path("private/functions/utility/nl2p.php"));
 include(base_path("classes/Database.php"));
 include(base_path("private/classes/FileUploader.php"));
 include(base_path("../lib/mustache.php-main/src/Mustache/Autoloader.php"));
@@ -14,23 +15,86 @@ use Database\Database;
 $db = new Database('admin');
 
 use FileUploader\FileUploader;
-$uploader = new FileUploader(CAROUSEL_ASSET_PATH, CAROUSEL_MAX_IMAGE_WIDTH, CAROUSEL_THUMBNAIL_WIDTH);
-$uploaded_files = $uploader->checkFileSizes()->uploadFiles()->getResponse();
-$uploaded_file = $uploaded_files[0];
 
-if ($uploaded_file["success"]) {
-    $media_id = updateCarouselDB($uploaded_file, $db);
+if (isset($_POST['create'])) {
+    $uploader = new FileUploader(CAROUSEL_ASSET_PATH, CAROUSEL_MAX_IMAGE_WIDTH, CAROUSEL_THUMBNAIL_WIDTH);
+    $uploaded_files = $uploader->checkFileSizes()->uploadFiles()->getResponse();
+    $uploaded_file = $uploaded_files[0];
+    
+    if ($uploaded_file["success"]) {
+        $media_id = createCarouselDB($uploaded_file, $db);
+    }
+    header("HX-Trigger: carouselUpdated");
+    echo $m->render("carouselForm", ["uploaded_file"=>$uploaded_file]);
 }
 
-echo $m->render("carouselForm", ["uploaded_file"=>$uploaded_file]);
+if (isset($_POST['update'])) {
+    $post_arr = rearrayCarouselPost();
+    foreach ($post_arr as $post) {
+        updateCarouselDB($post, $db);
+    }
+    $carousel_tiles = getCarouselTiles($db);
+    echo $m->render("carouselEdit", ["carousel_tiles"=>$carousel_tiles, "updated"=>true]);
+}
+
+if (isset($_POST['delete'])) {
+    unlink(CAROUSEL_ASSET_PATH . "images/" . $_POST['img_url']);
+    $query = "DELETE FROM carousel WHERE carousel_id = ?;";
+    $params = [$_POST['carousel_id']];
+    $db->query($query, $params);
+    echo $m->render("carouselEdit", ["deleted"=>true]);
+}
 
 
-function updateCarouselDB($uploaded_file, $db) {
-    $query = "INSERT INTO carousel (img_url, tile_text) VALUES (?, ?);";
+function createCarouselDB($uploaded_file, $db) {
+    $query = "INSERT INTO carousel (img_url, tile_title, tile_text) VALUES (?, ?, ?);";
     $params = [
         $uploaded_file['filename'],
-        $_POST['tileText'][$uploaded_file['key']]
+        $_POST['tile_title'][$uploaded_file['key']],
+        $_POST['tile_text'][$uploaded_file['key']]
     ];
     $db->query($query, $params);
     return $db->lastInsertId();
+}
+
+function rearrayCarouselPost() {
+    $arr = [];
+    foreach ($_POST['carousel_id'] as $key => $value) {
+        $arr[] = [
+            'carousel_id'=>$value,
+            'tile_title'=>$_POST['title'][$key],
+            'tile_text'=>$_POST['text'][$key],
+            'tile_order'=>$_POST['tile_order'][$key]
+        ];
+    }
+    return $arr;
+}
+
+function updateCarouselDB($post, $db) {
+    $query = "UPDATE carousel SET tile_title = ?, tile_text = ?, tile_order = ? WHERE carousel_id = ?;";
+    $params = [
+        $post['tile_title'],
+        $post['tile_text'],
+        $post['tile_order'],
+        $post['carousel_id']
+    ];
+    $db->query($query, $params);
+}
+
+function getCarouselTiles($db) {
+    $query = "SELECT * FROM carousel ORDER BY tile_order ASC";
+    $carousel_tiles = $db->query($query)->fetchAll();
+
+    $order_options = [];
+    for ($a = 1; $a <= sizeof($carousel_tiles); $a++) {
+        $order_options[] = ["order"=>$a];
+    }
+
+    foreach ($carousel_tiles as &$tile) {
+        $tile['order_options'] = $order_options;
+        $tile['order_options'][$tile['tile_order']-1] = ["order"=>$tile['tile_order'], "selected"=>"selected"];
+        $tile['path'] = "/" . CAROUSEL_ASSET_PATH . "images/" . $tile['img_url'];
+    }
+
+    return $carousel_tiles;
 }
