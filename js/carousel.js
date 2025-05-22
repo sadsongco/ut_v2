be8f94a436e0aae63a924acd2debb382f9d0e8bb
Nigel Powell <nigel@thesadsongco.com>
2025-05-22 06:58:05 +0100

    Carousel displays correctly

--- js/carousel.js
+++ js/carousel.js
@@ -5,12 +5,16 @@ transitionTime = transitionTime.substring(0, transitionTime.length - 2);
 const carouselWaitTime = 5000;
 
 const startCarousel = () => {
+  const container = document.getElementById('carousel');
   slides = document.getElementsByClassName('carousel-item');
   if (slides.length == 1) {
-    const slide = slides[0];
-    slide.classList.add('active');
-    slide.style.opacity = '1';
-    slide.style.zIndex = '2';
+    const firstSlide = slides[0];
+    firstSlide.classList.add('active');
+    firstSlide.style.transition = 'none';
+    firstSlide.style.left = '0';
+    firstSlide.style.opacity = '1';
+    firstSlide.style.zIndex = '2';
+    container.style.minHeight = `${firstSlide.scrollHeight}px`;
     return;
   }
 
@@ -21,6 +25,15 @@ const startCarousel = () => {
   function removeActive(slide) {
     slide.classList.remove('active');
   }
+
+  let containerHeight = 0;
+  for (let slide of slides) {
+    if (slide.scrollHeight > containerHeight) {
+      containerHeight = `${slide.scrollHeight}px`;
+    }
+  }
+
+  container.style.minHeight = containerHeight;
 
   addActive(slides[0]);
   interval = setInterval(function () {
