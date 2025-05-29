5db375a260dfcdc8ed67e462280ba2afecdaa9ae
Nigel Powell <nigel@thesadsongco.com>
2025-05-29 07:57:34 +0100

    Refund, transaction list

--- private/functions/interface/orders/refund_transaction.php
+++ private/functions/interface/orders/refund_transaction.php
@@ -10,8 +10,18 @@ $db = new Database('orders');
 use SUCheckout\SUCheckout;
 $checkout = new SUCheckout();
 
-$checkout->refundTransaction($_GET['transaction_id'], $db);
+$checkout->refundTransaction($_GET['transaction_id']);
 $result = $checkout->getResponse();
+if ($result->status == "REFUNDED") {
+    $query = "UPDATE Transactions SET transaction_id = NULL WHERE transaction_id = ?";
+    $stmt = $db->query($query, [$_GET['transaction_id']]);
+    $rows = $db->rowCount($stmt);
+    if ($rows == 0) {
+        $result->db_message = "Couldn't find order to update in database";
+    } else {
+        $result->db_message = "Updated order in database";
+    }
+}
 
 header("HX-Trigger: transactionListUpdated");
 echo '<div id="updatedResult" hx-swap-oob="true">
