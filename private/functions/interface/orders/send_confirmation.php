3d28c167210a682a4024b942cba42f84776845a6
Nigel Powell <nigel@thesadsongco.com>
2025-06-10 09:08:19 +0100

    Order management resend confirmation email

---
+++ private/functions/interface/orders/send_confirmation.php
@@ -0,0 +1,70 @@
+<?php
+
+include(__DIR__ . "/includes/order_includes.php");
+require (base_path("/functions/utility/create_unique_token.php"));
+require (base_path("/functions/utility/send_customer_email.php"));
+require (base_path("/functions/utility/create_order_pdf.php"));
+require (base_path('functions/shop/get_cart_contents.php'));
+require (base_path('functions/shop/make_order_pdf.php'));
+//Load Composer's autoloader
+require (base_path('../lib/vendor/autoload.php'));
+
+
+if (!isset($_GET['order_id'])) exit("No Order ID");
+
+$order_db_id = $_GET['order_id'];
+
+$shipping_items = [];
+$download_items = [];
+$preorder_items = [];
+$shipping_all = false;
+
+$query = "SELECT * FROM New_Orders WHERE order_id = ?";
+$order = $db->query($query, [$order_db_id])->fetch();
+
+if (!isset($_SESSION['items'])) $_SESSION['items'] = [];
+foreach ($_SESSION['items'] AS $item) {
+    updateItemData($item, $db);
+    classifyItem($item, $order_db_id, $db, $shipping_items, $download_items, $preorder_items);
+}
+
+
+if (!isset($_SESSION['bundles'])) $_SESSION['bundles'] = [];
+foreach($_SESSION['bundles'] AS $bundle) {
+    foreach ($bundle['items'] AS $item) {
+        updateItemData($item, $db);
+        classifyItem($item, $order_db_id,$db, $shipping_items,$download_items, $preorder_items);
+    }
+}
+
+if (!empty($shipping_items) && empty($preorder_items)) $shipping_all = true;
+if (!empty($shipping_items) && !empty($preorder_items)) $preorder_items['held'] = [...$shipping_items];
+
+$query = "SELECT customer_id FROM New_Orders WHERE order_id = ?";
+$customer_id = $db->query($query, [$order_db_id])->fetch()['customer_id'];
+$customer_token = createUniqueToken($customer_id);
+
+try {
+    $query = "SELECT
+            CONCAT(DATE_FORMAT(New_Orders.order_date, '%y%m%d'), '-', New_Orders.order_id) AS order_id,
+            Shipping_methods.service_name,
+            Customers.name,
+            Customers.email
+        FROM New_Orders
+        JOIN Customers ON New_Orders.customer_id = Customers.customer_id
+        JOIN Shipping_methods ON New_Orders.shipping_method = Shipping_methods.shipping_method_id
+        WHERE New_Orders.order_id = ?";
+    $order = $db->query($query, [$order_db_id])->fetch();
+
+    $order['shipping_items'] = $shipping_all;
+    $order['download_items'] = $download_items;
+    $order['preorder_items'] = $preorder_items;
+
+    sendCustomerEmail($order, "success", $db, $m);
+}
+catch (Exception $e) {
+    echo $e->getMessage();
+    exit();
+}
+
+echo "Confirmation email sent to " . $order['email'];
