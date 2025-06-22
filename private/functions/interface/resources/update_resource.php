c9bab4b68fa93bbfb821da290fc482b21181f615
Nigel Powell <nigel@thesadsongco.com>
2025-06-22 09:32:31 +0100

    Resources meta files save correctly

--- private/functions/interface/resources/update_resource.php
+++ private/functions/interface/resources/update_resource.php
@@ -3,32 +3,37 @@
 include(__DIR__ . "/../../../../functions/functions.php");
 include_once("includes/resource_includes.php");
 include_once(base_path("private/classes/FileUploader.php"));
+use FileUploader\FileUploader;
+
 
 if(!isset($_POST['resource_dir'])) {
     echo $m->render("partials/resourceForm", ["dir"=>$_POST["resource_dir"], "error"=>["message"=>"No resource directory specified. Could be that the file is too big to upload and have to be uploaded manually"]]);
     exit();
 }
 
-use FileUploader\FileUploader;
-$uploader = new FileUploader(RESOURCE_ASSET_PATH . $_POST['resource_dir'], true);
-if ($_POST['resource_dir'] == "logos") {
-    $uploader = new FileUploader(RESOURCE_ASSET_PATH . $_POST['resource_dir'], true, false, false);
-}
-$uploaded_files = $uploader->checkFileSizes()->getResponse();
+$uploaded_files = false;
 
-if (isset($uploaded_files["success"]) && !$uploaded_files["success"]) {
-    echo $m->render("partials/resourceForm", ["dir"=>$_POST["resource_dir"], "error"=>$uploaded_files]);
-    exit();
-}
-
-$uploaded_files = $uploader->uploadFiles()->getResponse();
-
-if (isset($uploaded_files[0]["success"]) && !$uploaded_files[0]["success"]) {
-        echo $m->render("partials/resourceForm", ["dir"=>$_POST["resource_dir"], "error"=>$uploaded_files[0]]);
+if (!empty($_FILES)) {
+    $uploader = new FileUploader(RESOURCE_ASSET_PATH . $_POST['resource_dir'], true);
+    if ($_POST['resource_dir'] == "logos") {
+        $uploader = new FileUploader(RESOURCE_ASSET_PATH . $_POST['resource_dir'], true, false, false);
+    }
+    $uploaded_files = $uploader->checkFileSizes()->getResponse();
+    
+    if (isset($uploaded_files["success"]) && !$uploaded_files["success"]) {
+        echo $m->render("partials/resourceForm", ["dir"=>$_POST["resource_dir"], "error"=>$uploaded_files]);
         exit();
+    }
+    $uploaded_files = $uploader->uploadFiles()->getResponse();
+    
+    if (isset($uploaded_files[0]["success"]) && !$uploaded_files[0]["success"]) {
+            echo $m->render("partials/resourceForm", ["dir"=>$_POST["resource_dir"], "error"=>$uploaded_files[0]]);
+            exit();
+    }
+    $_POST['file'] = $uploaded_files[0]['filename'];
 }
 
-$_POST['file'] = $uploaded_files[0]['filename'];
+
 
 $fields = null;
 if (isset($_POST["meta_filename"]) && $_POST["meta_filename"] != "") {
