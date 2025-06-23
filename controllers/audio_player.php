0f82bd9716ef54f7534c36443805b20ce124f3f3
Nigel Powell <nigel@thesadsongco.com>
2025-06-23 10:21:57 +0100

    audio serve path updated to work with preview in admin page

--- controllers/audio_player.php
+++ controllers/audio_player.php
@@ -14,6 +14,6 @@ $track = $_POST; //VALIDATE?
 $track["host"] = getHost();
 $track["title"] = str_replace("_", " ", $track["title"]);
 $track["notes"] = str_replace("_", " ", nl2br($track["notes"]));
-$track["path"] = "serve/" . ARTICLE_ASSET_PATH . "audio/" . str_replace(".", "/", $track["filename"]);
+$track["path"] = "/serve/" . ARTICLE_ASSET_PATH . "audio/" . str_replace(".", "/", $track["filename"]);
 
 echo $m->render("audio_track", $track);
