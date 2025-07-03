14a49db1143b2615a7db0c73f7d236eb303cbbd3
Nigel Powell <nigel@thesadsongco.com>
2025-07-03 22:07:09 +0100

    Resources debug message removed

--- functions/interface/resources/get_resource.php
+++ functions/interface/resources/get_resource.php
@@ -57,7 +57,6 @@ function getPressShotList($path) {
         ];
     }
     foreach ($photos as &$photo) {
-        p_2(base_path(RESOURCE_ASSET_PATH . $path."full_res/".$photo["resource"]));
         $full_res_size = getimagesize(base_path(RESOURCE_ASSET_PATH . $path."full_res/".$photo["resource"]));
         $photo["full_res_width"] = $full_res_size[0];
         $photo["full_res_height"] = $full_res_size[1];
