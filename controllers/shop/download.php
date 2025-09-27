7a5d35dea13acdfa712bd7092e3d0a6e9d986aaf
Nigel Powell <nigel@thesadsongco.com>
2025-09-27 10:40:40 +0100

    Force download functionality

--- controllers/shop/download.php
+++ controllers/shop/download.php
@@ -3,6 +3,7 @@
 include_once(__DIR__ . "/../../functions/functions.php");
 include_once(base_path("functions/shop/get_categories.php"));
 require(base_path("functions/utility/decrypt_token.php"));
+require(base_path("functions/utility/create_unique_token.php"));
 require(base_path("functions/utility/trigger_download.php"));
 
 use Database\Database;
@@ -14,6 +15,20 @@ if (!$download_token) exit("No Token");
 
 $id = decryptUniqueToken($download_token);
 if (!$id || !is_numeric($id)) exit("Invalid Token");
+
+if ($id == 7787782550) {
+    $order_no = isset($paths[3]) ? $paths[3] : false;
+    if(!$order_no) exit("No Order Number");
+    $query = "SELECT Items.download
+        FROM New_Orders
+        JOIN New_Order_items ON New_Orders.order_id = New_Order_items.order_id
+        JOIN Items ON New_Order_items.item_id = Items.item_id
+        WHERE New_Orders.order_id = ?";
+    $item = $db->query($query, [$order_no])->fetch();
+    $file_path = base_path(DOWNLOAD_PATH . $item['download']);
+    triggerDownload($item['download'], $file_path);
+    exit();
+}
 
 $query = "SELECT download
     FROM Download_tokens
