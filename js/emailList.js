929ca74c48c83e293b5000595e1f7f03d0187d34
Nigel Powell <nigel@thesadsongco.com>
2025-10-15 10:32:59 +0100

    email list join repaired

--- js/emailList.js
+++ js/emailList.js
@@ -1,38 +1,37 @@
 import { validEmail, submitEmail } from './modules/loader.js';
 
 // add event listeners for form submission
-window.onload = () => {
-  const submit = document.getElementById('emailSubmit');
+
+const submit = document.getElementById('emailSubmit');
+submit.disabled = true;
+
+document.getElementById('emailList').addEventListener('submit', async (e) => {
+  e.preventDefault();
   submit.disabled = true;
+  submit.value = '... processing';
+  const res = await submitEmail(e.target);
 
-  document.getElementById('emailList').addEventListener('submit', async (e) => {
-    e.preventDefault();
+  if (res.status == 'db_error') {
+    submit.value = 'there was an error, please try again';
+    submit.disabled = false;
+    return;
+  }
+  if (res.status == 'exists') {
+    submit.value = `you're already on the list!`;
+  }
+  if (res.success) {
+    submit.value = 'thank you! check email for confirmation';
+  }
+  document.getElementById('email').disabled = true;
+  document.getElementById('name').disabled = true;
+});
+
+document.getElementById('email').addEventListener('input', (e) => {
+  if (validEmail(e.target.value)) {
+    submit.disabled = false;
+    submit.value = 'join the list';
+  } else {
     submit.disabled = true;
-    submit.value = '... processing';
-    const res = await submitEmail(e.target);
-
-    if (res.status == 'db_error') {
-      submit.value = 'there was an error, please try again';
-      submit.disabled = false;
-      return;
-    }
-    if (res.status == 'exists') {
-      submit.value = `you're already on the list!`;
-    }
-    if (res.success) {
-      submit.value = 'thank you! check email for confirmation';
-    }
-    document.getElementById('email').disabled = true;
-    document.getElementById('name').disabled = true;
-  });
-
-  document.getElementById('email').addEventListener('input', (e) => {
-    if (validEmail(e.target.value)) {
-      submit.disabled = false;
-      submit.value = 'join the list';
-    } else {
-      submit.disabled = true;
-      submit.value = 'enter your email';
-    }
-  });
-};
+    submit.value = 'enter your email';
+  }
+});
