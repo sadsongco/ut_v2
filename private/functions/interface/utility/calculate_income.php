c78679ef66565f132e63c2a5f3db738524dc7266
Nigel Powell <nigel@thesadsongco.com>
2025-10-26 16:53:47 +0000

    Basic income calculation for tax year

--- private/functions/interface/utility/calculate_income.php
+++ private/functions/interface/utility/calculate_income.php
@@ -3,6 +3,7 @@
 include(__DIR__ . "/../../../../functions/functions.php");
 include(base_path("classes/Database.php"));
 include(base_path("../lib/mustache.php-main/src/Mustache/Autoloader.php"));
+include(__DIR__ . "/includes/make_income_pdf.php");
 
 use Database\Database;
 $db = new Database('orders');
@@ -16,19 +17,27 @@ $m = new Mustache_Engine(array(
 
 $period = isset($_POST['period']) ? $_POST['period'] : date("Y");
 $new_income = calculateNewIncome($db, $period);
+$new_income_non_vat = calculateNewIncome($db, $period, false);
 $old_income = calculateOldIncome($db, $period);
+$old_income_non_vat = calculateOldIncome($db, $period, false);
 
 $income = [
+    "period" => $period . " - " . ($period + 1),
     "subtotal" => number_format($new_income['subtotal'] + $old_income['subtotal'], 2),
     "shipping" => number_format($new_income['shipping'] + $old_income['shipping'], 2),
     "gross" => number_format($new_income['subtotal'] + $new_income['shipping'] - $new_income['vat'] + $old_income['subtotal'] + $old_income['shipping'] - $old_income['vat'], 2),
+    "vat_exempt_subtotal" => number_format($new_income_non_vat['subtotal'] + $old_income_non_vat['subtotal'], 2),
+    "vat_exempt_shipping" => number_format($new_income_non_vat['shipping'] + $old_income_non_vat['shipping'], 2),
     "vat" => number_format($new_income['vat'] + $old_income['vat'], 2),
-    "total" => number_format($new_income['total'] + $old_income['total'], 2)
+    "total" => number_format($new_income['total'] + $old_income['total'] + $new_income_non_vat['total'] + $old_income_non_vat['total'], 2)
 ];
 
-echo $m->render('income', ["income"=>$income, "period"=>$period]);
+$income_pdf_filename = makeIncomePDF($income, 'F', base_path(INCOME_PDF_PATH));
 
-function calculateNewIncome($db, $period) {
+echo $m->render('income', ["income"=>$income, "period"=>$period, "save_path"=>INCOME_PDF_PATH, "filename"=>$income_pdf_filename, "host"=>getHost()]);
+
+function calculateNewIncome($db, $period, $inc_vat = true) {
+    $area_cond = $inc_vat ? " AND country_id = 31 OR country_id = 215" : " AND country_id != 31 AND country_id != 215";
     try {
         $range_start = $period . "-04-05";
         $range_end = $period + 1 . "-04-04";
@@ -37,7 +46,11 @@ function calculateNewIncome($db, $period) {
             SUM(shipping) AS shipping,
             SUM(vat) AS vat,
             SUM(total) AS total
-        FROM New_Orders WHERE order_date BETWEEN ? AND ?";
+        FROM New_Orders
+        JOIN Customers ON New_Orders.customer_id = Customers.customer_id
+        JOIN Countries ON Customers.country = Countries.country_id
+        WHERE order_date BETWEEN ? AND ?
+        $area_cond;";
         $params = [$range_start, $range_end];
         $result = $db->query($query, $params)->fetch();
         return $result;
@@ -47,7 +60,8 @@ function calculateNewIncome($db, $period) {
     }
 }
 
-function calculateOldIncome($db, $period) {
+function calculateOldIncome($db, $period, $inc_vat = true) {
+    $area_cond = $inc_vat ? " AND country_id = 31 OR country_id = 215" : " AND country_id != 31 AND country_id != 215";
     try {
         $range_start = $period . "-04-05";
         $range_end = $period + 1 . "-04-04";
@@ -56,7 +70,11 @@ function calculateOldIncome($db, $period) {
             SUM(shipping) AS shipping,
             SUM(vat) AS vat,
             SUM(total) AS total
-        FROM Orders WHERE order_date BETWEEN ? AND ?";
+        FROM Orders
+        JOIN Customers ON Orders.customer_id = Customers.customer_id
+        JOIN Countries ON Customers.country = Countries.country_id
+        WHERE order_date BETWEEN ? AND ?
+        $area_cond;";
         $params = [$range_start, $range_end];
         $result = $db->query($query, $params)->fetch();
         return $result;
